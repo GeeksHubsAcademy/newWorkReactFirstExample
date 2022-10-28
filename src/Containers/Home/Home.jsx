30be78fbedfa73977ee39c7b8fc9579e57145c7d
Dave86dev <davidochando1986@gmail.com>
2022-10-28 13:48:45 +0200

    Conditional interpolation added

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -2,6 +2,7 @@
 //Basic JSX Component functional structure
 
 import React from 'react';
+import {useState, useEffect} from 'react';
 import Footer from '../../Components/Footer/Footer';
 import Header from '../../Components/Header/Header';
 
@@ -9,12 +10,33 @@ import './Home.css';
 
 const Home = () => {
 
+    let day = "Friday";
+    
+    //Hooks are declared here, they are the first to be checked by the component.
+    const [semaphore, setSemaphore] = useState(false);
+
+    const showText = () => {
+        //This is executed only when we click..
+
+        //We change the state of our component here,, just by adjusting the hook value.
+        setSemaphore(true);
+    }
+
+    //The render zone is the second thing that is being checked.
     return (
         <div className="homeDesign">
             <Header/>
             
             <div className="homeFiller">
-                I am the main content for the body of Home
+                {/* Conditional interpolation */}
+                {
+                    semaphore === true &&
+
+                    <div>This is Home and today is {day}</div>
+                }
+
+                <div onClick={()=>showText()}>click me.</div>
+                
             </div>
 
             <Footer/>
